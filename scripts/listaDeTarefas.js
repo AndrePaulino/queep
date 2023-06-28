e30154e5922cd1b5497420f8775b51ce101c921f
Andre Paulino <andrepaulinobtw@gmail.com>
2023-06-28 18:17:08 -0300

    🗞️ Add 'ConcluirBtn'

--- scripts/listaDeTarefas.js
+++ scripts/listaDeTarefas.js
@@ -16,7 +16,21 @@ function novaTarefa() {
 	const novaTarefaConteudo = `<p class="content">${novaTarefaTexto}</p>`;
 
 	novaTarefa.innerHTML = novaTarefaConteudo;
+	novaTarefa.appendChild(ConcluirBtn());
 	listaTarefasUl.appendChild(novaTarefa);
 
 	novaTarefaInput.value = "";
 }
+
+const ConcluirBtn = () => {
+	const concluirBtn = document.createElement("button");
+	concluirBtn.classList.add("check-button");
+	concluirBtn.innerText = "Concluir";
+	
+	concluirBtn.addEventListener("click", (event) => {
+		const tarefaConcluida = event.currentTarget.parentElement;
+		tarefaConcluida.classList.toggle("done");
+	});
+
+	return concluirBtn;
+};
